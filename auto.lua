69164621de4f2f3c89447f3ea7760bd7a7ad45ac
Kai <139547161+KaiYoshida1@users.noreply.github.com>
2025-06-17 23:42:07 +0800

    Auto update with PlaceID 126884695634066

--- auto.lua
+++ auto.lua
@@ -1,14 +1,3 @@
--- latestserver.lua
--- WARNING: Do NOT share this file with others
-
 local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
-
--- Replace with the actual server info
--- This will be overwritten by your bot whenever @everyone messages come in
-local placeId = 0
-local jobId = "00000000-0000-0000-0000-000000000000"
-
-if placeId > 0 and jobId ~= "" then
-    TeleportService:TeleportToPlaceInstance(placeId, jobId, Players.LocalPlayer)
-end
+TeleportService:TeleportToPlaceInstance(126884695634066, "e743af48-d6f3-41c3-bb0c-12fa218edce8", Players.LocalPlayer)
