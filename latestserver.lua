cc2430dadc8f2e9997d01cd85f147e049ad1f863
Kai <139547161+KaiYoshida1@users.noreply.github.com>
2025-06-18 11:24:51 +0800

    Update to job dec24b84-ba2d-421f-a31e-6ceda8c9e46d

--- latestserver.lua
+++ latestserver.lua
@@ -1,4 +1,4 @@
--- This file will be updated automatically by the bot.
+-- latestserver.lua
 local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
--- No teleport yet
+TeleportService:TeleportToPlaceInstance(126884695634066, "dec24b84-ba2d-421f-a31e-6ceda8c9e46d", Players.LocalPlayer)
