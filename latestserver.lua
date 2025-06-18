c7a86be055059d162908aca5d4020ff6afbde21f
Kai <139547161+KaiYoshida1@users.noreply.github.com>
2025-06-18 12:58:00 +0800

    Update to job fc4c9bd0-04c6-4114-b15c-d9fcc6a76e0a

--- latestserver.lua
+++ latestserver.lua
@@ -1,4 +1,4 @@
 -- latestserver.lua
 local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
-TeleportService:TeleportToPlaceInstance(126884695634066, "d956c123-2960-4dd1-8f3a-f61da37e699a", Players.LocalPlayer)
+TeleportService:TeleportToPlaceInstance(126884695634066, "fc4c9bd0-04c6-4114-b15c-d9fcc6a76e0a", Players.LocalPlayer)
